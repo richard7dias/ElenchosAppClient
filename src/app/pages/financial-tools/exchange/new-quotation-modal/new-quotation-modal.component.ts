b1b938294d860612baf5d0e9b6165fface804223
Richard Oliveira <richard.097@hotmail.com>
2023-08-30 17:47:34 -0300

    .

--- src/app/pages/financial-tools/exchange/new-quotation-modal/new-quotation-modal.component.ts
+++ src/app/pages/financial-tools/exchange/new-quotation-modal/new-quotation-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-new-quotation-modal',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class NewQuotationModalComponent {
 
+  quoteFrom!: string;
+  quoteFor!: string;
+  quotationValue!: number;
+  check!: string;
+
+  constructor(public modalRef: MatDialogRef<NewQuotationModalComponent>) { }
+
+  ngDoCheck() {
+    if (this.quoteFrom && this.quoteFor && this.quotationValue) {
+      this.check = `1 ${this.quoteFrom} é igual a ${this.quotationValue} (${this.quoteFor}).`;
+    }
+  }
+
+  submitForm() {
+    const newQuotation = {
+      idOwner: 'pegar o id aqui',
+      check: this.check,
+      quotationValue: this.quotationValue,
+      quoteFor: this.quoteFor,
+      quoteFrom: this.quoteFrom
+    }
+
+    //implementar aqui a lógica para salvar na api com o guid do usuário logado
+
+    this.modalRef.close(true);
+  }
 }
