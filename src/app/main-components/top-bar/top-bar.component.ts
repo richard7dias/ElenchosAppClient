63b786c71ec3fc437f0679a934acf42ab2e7717c
Richard Oliveira <richard.097@hotmail.com>
2023-09-17 23:20:02 -0300

    .

--- src/app/main-components/top-bar/top-bar.component.ts
+++ src/app/main-components/top-bar/top-bar.component.ts
@@ -5,6 +5,9 @@ import { NumberService } from 'src/app/shared/formatting/number.service';
 import { InternalCashService } from 'src/app/shared/internal-values/internal-cash/internal-cash.service';
 import { ApiBalancesService } from 'src/app/core/api/balances/api-balances.service';
 import { HttpResponse } from '@angular/common/http';
+import { ApiSourceExpenseService } from 'src/app/core/api/source-expense/api-source-expense.service';
+import { InternalBalancesService } from 'src/app/shared/internal-values/internal-balances/internal-balances.service';
+import { InternalExpensesService } from 'src/app/shared/internal-values/internal-expenses/internal-expenses.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -20,19 +23,37 @@ export class TopBarComponent implements OnInit {
     private _internalUser: InternalUserService,
     private _internalCash: InternalCashService,
     private _router: Router,
-    private _numberFormat: NumberService,
-
-    private _apiBalances: ApiBalancesService
+    private _apiBalances: ApiBalancesService,
+    private _apiExpenses: ApiSourceExpenseService,
+    private _internalBalances: InternalBalancesService,
+    private _internalExpenses: InternalExpensesService,
   ) { }
 
   ngOnInit() {
     this._internalUser.getInternalUser().subscribe(user => {
       this.userName = user?.firstName;
+      if (user) {
+        this.cashCalculate();
+      }
     });
 
     this._internalCash.getInternalCash().subscribe(cash => {
       this.internalCash = cash;
     });
+  }
+
+  cashCalculate() {
+    this._apiBalances.getBalances().subscribe(
+      (response: HttpResponse<any>) => {
+        this._internalBalances.setInternalBalances(response.body);
+      }
+    );
+
+    this._apiExpenses.getSourceExpenses().subscribe(
+      (response: HttpResponse<any>) => {
+        this._internalExpenses.setInternalExpenses(response.body);
+      }
+    );
   }
 
   logout() {
