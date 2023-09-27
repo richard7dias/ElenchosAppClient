d935a233cd862773a5d856b9cccdb7b1db0f73ee
Richard Oliveira <richard.097@hotmail.com>
2023-09-27 16:28:24 -0300

    .

--- src/app/shared/internal-values/internal-cash/internal-cash.service.ts
+++ src/app/shared/internal-values/internal-cash/internal-cash.service.ts
@@ -1,19 +1,10 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
-
 import { NumberService } from '../../formatting/number/number.service';
 import { InternalBalancesService } from '../internal-balances/internal-balances.service';
 import { InternalExpensesService } from '../internal-expenses/internal-expenses.service';
 import { Balance } from 'src/app/core/interfaces/balance.interface';
 import { SourceExpense } from 'src/app/core/interfaces/sourceExpense.interface';
-import { InternalMonthlyCalculationsService } from '../internal-monthly-calculations/internal-monthly-calculations.service';
-import { InternalLaunchesService } from '../internal-launches/internal-launches.service';
-import { InternalCategoriesService } from '../internal-categories/internal-categories.service';
-import { ApiLaunchesService } from 'src/app/core/api/launches/api-launches.service';
-import { ApiCategoriesService } from 'src/app/core/api/categories/api-categories.service';
-import { HttpResponse } from '@angular/common/http';
-import { Category } from 'src/app/core/interfaces/category.interface';
-import { Launch } from 'src/app/core/interfaces/launch.interface';
 
 
 @Injectable({
@@ -30,11 +21,6 @@ export class InternalCashService {
     private _numberFormat: NumberService,
     private _internalBalances: InternalBalancesService,
     private _internalExpenses: InternalExpensesService,
-    private _internalMonthlyCalculations: InternalMonthlyCalculationsService,
-    private _internalLaunches: InternalLaunchesService,
-    private _internalCategories: InternalCategoriesService,
-    private _apiLaunches: ApiLaunchesService,
-    private _apiCategories: ApiCategoriesService
   ) {
     this.subscribeBalancesAndExpenses();
 
@@ -49,7 +35,7 @@ export class InternalCashService {
       });
   }
 
-  private setInternalCash(sumTotal: number) {
+  setInternalCash(sumTotal: number) {
     this.internalCash.next(this._numberFormat.inPortToDuo(sumTotal));
   }
 
@@ -73,10 +59,6 @@ export class InternalCashService {
     let sumExpenses: number = 0;
     let sumTotal: number = 0;
 
-    this._internalMonthlyCalculations.getInternalTotalAvailable().subscribe(total => {
-      sumExpenses += total;
-    });
-
     if (this.internalBalances && this.internalBalances.length > 0) {
       sumBalances = this.internalBalances
         .map(obj => obj.valueBalance)
@@ -84,7 +66,7 @@ export class InternalCashService {
     }
 
     if (this.internalExpenses && this.internalExpenses.length > 0) {
-      sumExpenses += this.internalExpenses
+      sumExpenses = this.internalExpenses
         .map(obj => obj.valueExpense)
         .reduce((acc, value) => acc + value, 0);
     }
@@ -92,26 +74,5 @@ export class InternalCashService {
     sumTotal = sumBalances - sumExpenses;
 
     this.setInternalCash(sumTotal);
-  }
-
-  private loadingValueCurrentMonth(): void {
-    this._internalCategories.getInternalCategories().subscribe(categories => {
-      if (!categories) {
-        this._apiCategories.getCategories().subscribe(
-          (categoriesApi: HttpResponse<Category[]>) => {
-            this._internalCategories.setInternalCategories(categoriesApi.body);
-          }
-        );
-      }
-    });
-    this._internalLaunches.getInternalLaunches().subscribe(launches => {
-      if (!launches) {
-        this._apiLaunches.getLaunches().subscribe(
-          (launchesApi: HttpResponse<Launch[]>) => {
-            this._internalLaunches.setInternalLaunches(launchesApi.body);
-          }
-        );
-      }
-    });
   }
 }
